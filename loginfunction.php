89bd0b9a6c8439bb82a0ea8055664068723dccc2
Jastine Formentera <enitsajformentera.13@gmail.com>
2022-11-07 02:16:30 +0800

    updated login form

--- loginfunction.php
+++ loginfunction.php
@@ -76,7 +76,7 @@ if (isset($_POST['submit'])) {
             }
       
         } else {
-            $_SESSION['error'] = json_encode("Invalid username or password.");
+            $_SESSION['error'] = json_encode("Invalid email or password.");
         }
     }   
 }
